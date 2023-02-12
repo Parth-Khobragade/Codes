4f6756bcb4885c29663d5dc0d940a732310929e4
Parth Khobragade <122554378+parthkhobragade06@users.noreply.github.com>
2023-02-12 23:18:19 +0530

    Create The dice problem.java

---
+++ Geeks_For_Geeks/The dice problem.java
@@ -0,0 +1,8 @@
+class Solution{
+    static int oppositeFaceOfDice(int N){
+        // code here
+        return (7-N);
+    }
+}
+
+{https://practice.geeksforgeeks.org/problems/the-dice-problem2316/0}

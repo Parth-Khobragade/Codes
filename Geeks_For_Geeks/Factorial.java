acdfd406b99c2696fbc693c416119c4ae85e0df0
Parth Khobragade <122554378+parthkhobragade06@users.noreply.github.com>
2023-02-12 23:20:34 +0530

    Create Factorial.java

---
+++ Geeks_For_Geeks/Factorial.java
@@ -0,0 +1,12 @@
+class Solution{
+    static long factorial(int N){
+        // code here
+        long fact=1;
+        for(long i=1;i<=N;i++){
+            fact=fact*i;
+        }
+        return fact;
+    }
+}
+
+{https://practice.geeksforgeeks.org/problems/factorial5739/0}

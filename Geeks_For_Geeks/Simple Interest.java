d57185d6b8b3ad0b2f4e2411259744ffa1a948c2
Parth Khobragade <122554378+parthkhobragade06@users.noreply.github.com>
2023-02-12 23:21:35 +0530

    Create Simple Interest.java

---
+++ Geeks_For_Geeks/Simple Interest.java
@@ -0,0 +1,10 @@
+class Solution {
+    double simpleInterest(int P, int R, int T) {
+        // code here
+        return(P*R*T/100.0);
+    }
+}
+
+
+
+{https://practice.geeksforgeeks.org/problems/simple-interest3457/0}

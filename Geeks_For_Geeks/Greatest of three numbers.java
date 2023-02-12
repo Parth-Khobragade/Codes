c03d4092155da9753b88c70ddf6bf1eb956292f0
Parth Khobragade <122554378+parthkhobragade06@users.noreply.github.com>
2023-02-12 23:19:30 +0530

    Create Greatest of three numbers.java

---
+++ Geeks_For_Geeks/Greatest of three numbers.java
@@ -0,0 +1,17 @@
+class Solution {
+    int greatestOfThree(int A, int B, int C) {
+        // code here
+        if(A>B && A>C){
+            return A;
+        }
+        else if(B>C){
+            return B;
+        }
+        else{
+            return C;
+        }
+    }
+}
+
+
+{https://practice.geeksforgeeks.org/problems/greatest-of-three-numbers2520/0}

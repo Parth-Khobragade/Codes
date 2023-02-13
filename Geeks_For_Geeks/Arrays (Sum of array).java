e9421f127dd3587a422f206ecd34666b39f808e6
Parth Khobragade <122554378+parthkhobragade06@users.noreply.github.com>
2023-02-13 22:13:16 +0530

    Create Arrays (Sum of array).java

---
+++ Geeks_For_Geeks/Arrays (Sum of array).java
@@ -0,0 +1,15 @@
+class Solution {
+    
+    public long getSum(long a[], long n)
+    {
+        // Your code goes here
+        long sum=0;
+        for(int i=0;i<a.length;i++){
+            sum=sum+a[i];
+        }
+        return sum;
+        
+    }
+}
+
+{https://practice.geeksforgeeks.org/problems/c-arrays-sum-of-array-set-14805/1?page=1&difficulty[]=-2&category[]=Arrays&sortBy=submissions}

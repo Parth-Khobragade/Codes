6064e6c1f28df0f78b8edca3d3003d894652adeb
Parth Khobragade <122554378+parthkhobragade06@users.noreply.github.com>
2023-02-14 23:45:10 +0530

    Create Mean.java

---
+++ Geeks_For_Geeks/Mean.java
@@ -0,0 +1,13 @@
+
+class Solution {
+    static int mean(int N , int[] A) {
+        // code here
+        int sum=0;
+        for(int i=0;i<A.length;i++){
+            sum=sum+A[i];
+        }
+        return sum/N;
+    }
+};
+
+{https://practice.geeksforgeeks.org/problems/mean0021/1?page=4&difficulty[]=-2&sortBy=submissions}

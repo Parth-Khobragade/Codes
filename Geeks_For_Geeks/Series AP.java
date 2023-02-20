1a183f60d29d68ce38b45369fdbeea626fea688d
Parth Khobragade <122554378+parthkhobragade06@users.noreply.github.com>
2023-02-20 22:50:45 +0530

    Create Series AP.java

---
+++ Geeks_For_Geeks/Series AP.java
@@ -0,0 +1,8 @@
+class Solution {
+    int nthTermOfAP(int A1, int A2, int N) {
+        int d = A2-A1;
+        return A1+(N-1)*d;
+    }
+}
+
+{https://practice.geeksforgeeks.org/problems/series-ap5310/1?page=2&difficulty[]=-2&sortBy=submissions}

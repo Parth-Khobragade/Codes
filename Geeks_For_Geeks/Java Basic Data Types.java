7e8c552833fe3f47655d05113ec199ac217c4a87
Parth Khobragade <122554378+Parth-Khobragade@users.noreply.github.com>
2023-02-22 23:56:37 +0530

    Create Java Basic Data Types.java

---
+++ Geeks_For_Geeks/Java Basic Data Types.java
@@ -0,0 +1,20 @@
+class Solution {
+
+    int javaIntType(Scanner sc) {
+        // code here
+        return sc.nextInt();
+    }
+    
+    String javaStringType(Scanner sc) {
+        // code here
+        return sc.next();
+    }
+    
+    float javaFloatType(Scanner sc) {
+    
+        // code here
+        return sc.nextFloat();
+    }
+};
+
+{https://practice.geeksforgeeks.org/problems/java-basic-data-types0041/1?page=5&difficulty[]=-2&sortBy=submissions}

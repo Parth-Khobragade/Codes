66fb24d311446fff2a78f8a33c02d815226004ff
Parth Khobragade <122554378+Parth-Khobragade@users.noreply.github.com>
2023-02-22 23:51:37 +0530

    Create Multiply Array.java

---
+++ Geeks_For_Geeks/Multiply Array.java
@@ -0,0 +1,14 @@
+class Solution {
+
+    public static int product(int arr[],int n)
+    {
+        int p =1;
+        for (int i=0;i<n;i++)
+        {
+          p = p*arr[i];  
+        }
+        return p;
+    }
+}
+
+{https://practice.geeksforgeeks.org/problems/multiply-array-1658312632/1?page=5&difficulty[]=-2&sortBy=submissions}

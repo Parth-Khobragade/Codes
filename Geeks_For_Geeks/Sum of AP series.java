b24fb72a68cbfe0062161cd0f55e5683e43d8942
Parth Khobragade <122554378+Parth-Khobragade@users.noreply.github.com>
2023-02-27 23:20:40 +0530

    Create Sum of AP series.java

---
+++ Geeks_For_Geeks/Sum of AP series.java
@@ -0,0 +1,12 @@
+class Solution
+{
+    public long sum_of_ap(long n, long a, long d)
+    {
+        // Code here
+        
+        long result=(n*(2*a+(n-1)*d))/2;
+        return result;
+    }
+}
+
+{https://practice.geeksforgeeks.org/problems/sum-of-ap-series4512/1?page=5&difficulty[]=-2&sortBy=submissions}

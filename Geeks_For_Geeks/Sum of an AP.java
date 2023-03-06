692159fd6b9f899350596ddc9c4fb04110e4725d
Parth Khobragade <122554378+Parth-Khobragade@users.noreply.github.com>
2023-03-06 23:35:48 +0530

    Create Sum of an AP.java

---
+++ Geeks_For_Geeks/Sum of an AP.java
@@ -0,0 +1,12 @@
+class Solution
+{
+    public int sum_of_ap(int n, int a, int d)
+    {
+        // Code here
+        int sum=0;
+        sum=(n*(2*a+(n-1)*d))/2;
+        return sum;
+    }
+}
+
+{https://practice.geeksforgeeks.org/problems/sum-of-an-ap1025/1?page=6&difficulty[]=-2&sortBy=submissions}

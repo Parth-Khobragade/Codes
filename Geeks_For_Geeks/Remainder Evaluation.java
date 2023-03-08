f52b52a12f951c6e53045915bfcccb9c6061e867
Parth Khobragade <122554378+Parth-Khobragade@users.noreply.github.com>
2023-03-08 23:50:32 +0530

    Create Remainder Evaluation.java

---
+++ Geeks_For_Geeks/Remainder Evaluation.java
@@ -0,0 +1,9 @@
+class Solution{
+    static int findRemainder(int num1,int num2){
+        // code here
+       return (num1%num2); 
+    }
+    
+}
+
+{https://practice.geeksforgeeks.org/problems/remainder-evaluation3755/1?page=3&difficulty[]=-2&sortBy=submissions}

8f84c05ea1fa9be984e8cade097176e54bdc70ea
Parth Khobragade <122554378+Parth-Khobragade@users.noreply.github.com>
2023-03-10 23:29:48 +0530

    Create Vowel or Not.java

---
+++ Geeks_For_Geeks/Vowel or Not.java
@@ -0,0 +1,13 @@
+class Solution{
+    String isVowel(char c){
+        // code here
+       
+           if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u'  || c=='A' || c=='E' || c=='I' || c=='O' || c=='U')
+                return "YES";
+             else 
+             return "NO";
+        
+    }
+}
+
+{https://practice.geeksforgeeks.org/problems/vowel-or-not0831/1?page=4&difficulty[]=-2&sortBy=submissions}

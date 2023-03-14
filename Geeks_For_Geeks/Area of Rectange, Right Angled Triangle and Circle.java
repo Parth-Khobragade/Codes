a7e2ea735099cab5068c4da0e5f42999cd14f1fd
Parth Khobragade <122554378+Parth-Khobragade@users.noreply.github.com>
2023-03-14 23:29:44 +0530

    Create Area of Rectange, Right Angled Triangle and Circle.java

---
+++ Geeks_For_Geeks/Area of Rectange, Right Angled Triangle and Circle.java
@@ -0,0 +1,13 @@
+class Solution {
+    static int[] getAreas(int L , int W , int B , int H , int R) {
+        // code here
+        int AOR=L*W;
+        int AOT=(int)(0.5*B*H);
+        int AOC=(int)(3.14*R*R);
+        
+       return new int[]{AOR,AOT,AOC}; 
+    }
+    
+};
+
+{https://practice.geeksforgeeks.org/problems/area-of-rectange-right-angled-triangle-and-circle2600/1?page=6&difficulty[]=-2&sortBy=submissions}

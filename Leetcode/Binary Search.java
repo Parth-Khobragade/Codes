92226a62e2e0778d3285edb122652e3e219a7059
Parth Khobragade <122554378+Parth-Khobragade@users.noreply.github.com>
2023-06-19 23:45:42 +0530

    Create Binary Search.java

---
+++ Leetcode/Binary Search.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int start=0;
+        int end=nums.length-1;
+
+        while(start<=end){
+            int mid=start+(end-start)/2;
+
+            if(target< nums[mid]){
+               end=mid-1;
+            }
+            else if(target>nums[mid]){
+                start=mid+1;
+            }
+            else{
+                return mid;
+            }
+        }
+       return -1;
+    }
+}
+
+//https://leetcode.com/problems/binary-search/description/

b1f6cabd612bb1e41976a0125a7533fc4ab69b71
Parth Khobragade <122554378+Parth-Khobragade@users.noreply.github.com>
2023-06-28 23:07:44 +0530

    Create Fibonacci Number.java

---
+++ Leetcode/Fibonacci Number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int fib(int n) {
+        if(n<2){
+            return n;
+        }
+        return fib(n-1) + fib(n-2);
+    }
+}
+
+//https://leetcode.com/problems/fibonacci-number/description/

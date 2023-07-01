ac6f53d04c2687de9598073c2e251c41645646d2
Parth Khobragade <122554378+Parth-Khobragade@users.noreply.github.com>
2023-07-01 23:58:44 +0530

    Update Sqrt(x).java

--- Leetcode/Sqrt(x).java
+++ Leetcode/Sqrt(x).java
@@ -3,19 +3,18 @@ class Solution {
         if (x == 0) {
             return 0;
         }
-        int first = 1, last = x;
-        while (first <= last) {
-            int mid = first + (last - first) / 2;
+        int start = 1, end = x;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
             if (mid == x / mid) {
                 return mid;
             } else if (mid > x / mid) {
-                last = mid - 1;
+                end = mid - 1;
             } else {
-                first = mid + 1;
+                start = mid + 1;
             }
         }
-        return last;
+        return end;
     }
 }
-
 //https://leetcode.com/problems/sqrtx/description/

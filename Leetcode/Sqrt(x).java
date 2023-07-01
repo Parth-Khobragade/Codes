230ac63209eb3ddb44d4fdf5bfcf543396cb73e5
Parth Khobragade <122554378+Parth-Khobragade@users.noreply.github.com>
2023-07-01 23:57:08 +0530

    Create  Sqrt(x).java

---
+++ Leetcode/Sqrt(x).java
@@ -0,0 +1,21 @@
+class Solution {
+    public int mySqrt(int x) {
+        if (x == 0) {
+            return 0;
+        }
+        int first = 1, last = x;
+        while (first <= last) {
+            int mid = first + (last - first) / 2;
+            if (mid == x / mid) {
+                return mid;
+            } else if (mid > x / mid) {
+                last = mid - 1;
+            } else {
+                first = mid + 1;
+            }
+        }
+        return last;
+    }
+}
+
+//https://leetcode.com/problems/sqrtx/description/

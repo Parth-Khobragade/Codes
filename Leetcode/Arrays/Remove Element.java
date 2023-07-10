09a3fe827574e35a1ded27c9605e48a0444d7e55
Parth Khobragade <122554378+Parth-Khobragade@users.noreply.github.com>
2023-07-10 23:58:33 +0530

    Create Remove Element.java

---
+++ Leetcode/Arrays/Remove Element.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=val){
+             nums[count]=nums[i];
+             count++
+;            }
+        }
+        return count;
+    }
+}
+
+//https://leetcode.com/problems/remove-element/description/

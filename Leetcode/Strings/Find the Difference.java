91ca308f218d567cd31c2d1f16331e8b893834ba
Parth Khobragade <122554378+Parth-Khobragade@users.noreply.github.com>
2023-07-28 00:00:28 +0530

    Update Find the Difference.java

--- Leetcode/Strings/Find the Difference.java
+++ Leetcode/Strings/Find the Difference.java
@@ -1,1 +1,17 @@
-Find the Difference.java
+class Solution {
+    public char findTheDifference(String s, String t) {
+        int total=0;
+
+        for(int i=0;i<t.length();i++){
+         total = total + t.charAt(i);
+        }
+
+         for(int i=0;i<s.length();i++){
+         total = total - s.charAt(i);
+        }
+
+        return (char)total;
+    }
+}
+
+//https://leetcode.com/problems/find-the-difference/
